9f2428cc052003db93b9f103901f4466d1fd875a
Yuchao Wu <yuchao.sydneyuni@gmail.com>
2022-04-29 22:07:28 +1000

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { Teleport, Suspense } from 'vue'
+
+export default defineComponent({
+  components: { Teleport, Suspense},
+})
+</script>
+
+<template>
+  <component :is="Math.random() > 0.5 ? 'Teleport' : 'Suspense'" :to="body">
+    This is the real content  
+  </component> 
+</template>
+
